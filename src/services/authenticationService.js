6fbad73cddeeeb93bf560729b3dccd0f9d2797bb
James <jamesleahy314@gmail.com>
2018-05-01 22:43:21 -0700

    containerize frontend

--- src/services/authenticationService.js
+++ src/services/authenticationService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://6300694e.ngrok.io';
+const BASE_URL = 'http://127.0.0.1:8090';
 
 export {signUp, login, signOut, passwordReset, passwordChange, isAuthed};
 
