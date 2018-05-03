1bb7cbfe1e3675ebbe7056e8807f69990c2f9b39
James <jamesleahy314@gmail.com>
2018-05-03 13:59:26 -0700

    load ENV through REACT_APP variables, update docker-compose, and staging-dockerrun.aws.json to load env

--- src/services/authenticationService.js
+++ src/services/authenticationService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://127.0.0.1:8090';
+const BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
 export {signUp, login, signOut, passwordReset, passwordChange, isAuthed};
 
